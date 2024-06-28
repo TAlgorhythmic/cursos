235ff4c93edaa89d36b52b610990142453c7809b
Algorhythmics <intivioli@gmail.com>
2024-06-28 19:04:17 +0200

    Create Page Implementation.

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,9 +1,46 @@
+import { Button } from "bootstrap";
 import "./style.css";
+import { useState } from "react";
 
 export default function Create() {
 
-    function submit() {
+    const [isValid, setValid] = useState([true, ""]);
 
+    function submit() {
+        const title = document.getElementById("title").value;
+        const description = document.getElementById("description").value;
+        const price = document.getElementById("price").value;
+        const img = document.getElementById("img").value;
+        const rating = document.getElementById("rating").value;
+        
+        if (title.length > 16) {
+            setValid([false], "El título no puede ser más largo de 16 carácteres.")
+            return;
+        }
+        if (!) {
+            
+        }
+    }
+
+    const validStyle = {
+        height: "0px",
+        width: "0px",
+        margin: "0px"
+    }
+    const invalidStyle = {
+        height: "100px",
+        width: "100%",
+        margin: "0px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "red",
+        color: "white",
+        fontSize: "larger"
+    }
+
+    const override = {
+        marginRight: "0px !important"
     }
 
     return (
@@ -11,19 +48,27 @@ export default function Create() {
             <div className="form-box">
                 <div className="field">
                     <label className="inner-item" htmlFor="title">Título</label>
-                    <input className="inner-item" id="title" type="text"/>
-
+                    <input className="inner-item" id="title" type="text" />
+                </div>
+                <div className="field">
                     <label className="inner-item" htmlFor="description">Descripción</label>
-                    <input className="inner-item" id="description" type="textarea"/>
-
-                    <label className="inner-item" htmlFor="title">Precio</label>
-                    <input className="inner-item" id="title" type="text"/>
-
-                    <label className="inner-item" htmlFor="title">Título</label>
-                    <input className="inner-item" id="title" type="text"/>
-
-                    <label className="inner-item" htmlFor="title">Título</label>
-                    <input className="inner-item" id="title" type="text"/>
+                    <input className="inner-item" id="description" type="textarea" />
+                </div>
+                <div className="field">
+                    <label className="inner-item" htmlFor="price">Precio</label>
+                    <input className="inner-item" id="price" type="number" />
+                </div>
+                <div className="field">
+                    <label className="inner-item" htmlFor="img">Imágen (URL)</label>
+                    <input className="inner-item" id="img" type="text" />
+                </div>
+                <div className="field">
+                    <label className="inner-item" htmlFor="rating">Rating</label>
+                    <input className="inner-item" style={override} id="rating" type="text"/>
+                    <p className="inner-item">/5</p>
+                </div>
+                <div className="field">
+                    <button className="button" onClick={submit}>Crear</button>
                 </div>
             </div>
         </div>
