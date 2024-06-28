0f1a85195f1aacf8c08d1fd77044084e853356e8
Algorhythmics <intivioli@gmail.com>
2024-06-28 18:34:56 +0200

    Update.

--- src/Create.jsx
+++ src/Create.jsx
@@ -1,8 +1,31 @@
+import "./style.css";
+
 export default function Create() {
-    
+
+    function submit() {
+
+    }
+
     return (
-        <>
-            
-        </>
+        <div className="container-centered">
+            <div className="form-box">
+                <div className="field">
+                    <label className="inner-item" htmlFor="title">Título</label>
+                    <input className="inner-item" id="title" type="text"/>
+
+                    <label className="inner-item" htmlFor="description">Descripción</label>
+                    <input className="inner-item" id="description" type="textarea"/>
+
+                    <label className="inner-item" htmlFor="title">Precio</label>
+                    <input className="inner-item" id="title" type="text"/>
+
+                    <label className="inner-item" htmlFor="title">Título</label>
+                    <input className="inner-item" id="title" type="text"/>
+
+                    <label className="inner-item" htmlFor="title">Título</label>
+                    <input className="inner-item" id="title" type="text"/>
+                </div>
+            </div>
+        </div>
     )
 }
