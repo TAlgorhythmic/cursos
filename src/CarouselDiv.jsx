3c7f462e3c61638467062031647bb815ad0c5c26
Nayra Rios <nayrios2004@gmail.com>
2024-07-10 19:57:56 +0200

    ddd

--- src/CarouselDiv.jsx
+++ src/CarouselDiv.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
 export default function CarouselDiv(props) {
     const img = props.img;
     const title = props.title;
     const description = props.description;
+    const price = props.price;
+
+    const [total, setTotal] = useState(0);
+
+    function handleCompra(){
+        const sumaTotal = price + total;
+        setTotal(sumaTotal);
+    }
+    function addToCart(){
+        handleCompra();
+    }
+
     return (
         <div className="full-content">
             <img src={img} className="full-content-image" />
@@ -9,6 +23,7 @@ export default function CarouselDiv(props) {
                 <h2>{title}</h2>
                 <p className="margin">{description}</p>
             </div>
+          <button onClick={handleCompra}>Add to CART</button>   
         </div>
     )
 }
